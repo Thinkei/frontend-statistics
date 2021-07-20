5a3d53a4f7e148132f838d65fbe882f4e20a0e9d
Nguyễn Đặng Thế Quang <quang.nguyen@employmenthero.com>
2021-07-20 17:30:48 +0700

    Fix for loop, add key
    Replace for loop with map,
    add key props

--- src/components/DrawTable.js
+++ src/components/DrawTable.js
@@ -1,20 +1,17 @@
 const DrawTable = () => {
   const data = require('../data/2021-07-08.json')['@ehrocks/react-velonic'];
-  const velonic = [];
-  const dataKey = Object.keys(data);
+  const velonicComponents = Object.keys(data);
 
-  dataKey.map(key =>
-    velonic.push({
-      name: key,
-      freq: data[key],
-    })
-  );
+  const velonicData = velonicComponents.map(key => ({
+    name: key,
+    freq: data[key],
+  }));
 
-  const renderTableData = velonic.map((d, i) => (
-    <tr>
-      <td> {i + 1} </td>
-      <td> {d.name}</td>
-      <td>{d.freq}</td>
+  const renderTableData = velonicData.map((rowData, idx) => (
+    <tr key={rowData.name}>
+      <td> {idx + 1} </td>
+      <td> {rowData.name}</td>
+      <td>{rowData.freq}</td>
     </tr>
   ));
 
@@ -22,11 +19,13 @@ const DrawTable = () => {
     <div className="freq-table">
       <h2>Velonic components that still be used</h2>
       <table>
-        <tr>
-          <th> Index </th>
-          <th> In used Velonic </th>
-          <th> Freq </th>
-        </tr>
+        <tbody>
+          <tr>
+            <th> Index </th>
+            <th> In used Velonic </th>
+            <th> Freq </th>
+          </tr>
+        </tbody>
 
         <tbody>{renderTableData}</tbody>
       </table>
